15ec8692897462ac8a490982ebeab305a912f2d8
Sergio del Amo <sergio.delamo@softamo.com>
2022-07-19 08:39:32 +0200

    add turbo

--- http/src/main/java/com/micronauttodo/controllers/TodoDeleteController.java
+++ http/src/main/java/com/micronauttodo/controllers/TodoDeleteController.java
@@ -1,19 +1,25 @@
 package com.micronauttodo.controllers;
 
 import com.micronauttodo.persistence.OAuthUser;
+import com.micronauttodo.persistence.Todo;
 import com.micronauttodo.persistence.TodoRepository;
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.uri.UriBuilder;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import io.micronaut.views.turbo.TurboStream;
+import io.micronaut.views.turbo.http.TurboMediaType;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Collections;
 
 @Controller
 class TodoDeleteController {
@@ -27,9 +33,16 @@ class TodoDeleteController {
     @Post("/todo/{id}/delete")
     @Secured(SecurityRule.IS_AUTHENTICATED)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_HTML)
     HttpResponse<?> delete(@NonNull @NotBlank @PathVariable String id,
-                           @NonNull OAuthUser oAuthUser) {
+                           @NonNull OAuthUser oAuthUser,
+                           HttpRequest<?> request) {
         repository.delete(id, oAuthUser);
+        if (TurboMediaType.acceptsTurboStream(request)) {
+            return HttpResponse.ok(TurboStream.builder()
+                    .targetDomId("todo-" + id)
+                    .remove());
+        }
         return HttpResponse.seeOther(UriBuilder.of("/todo").build());
     }
 }
--- http/src/main/java/com/micronauttodo/controllers/TodoSaveController.java
+++ http/src/main/java/com/micronauttodo/controllers/TodoSaveController.java
@@ -1,22 +1,28 @@
 package com.micronauttodo.controllers;
 
 import com.micronauttodo.persistence.OAuthUser;
+import com.micronauttodo.persistence.Todo;
 import com.micronauttodo.persistence.TodoCreate;
 import com.micronauttodo.persistence.TodoSaveService;
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.uri.UriBuilder;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import io.micronaut.views.turbo.TurboStream;
+import io.micronaut.views.turbo.http.TurboMediaType;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Collections;
 
 @Controller
 public class TodoSaveController {
@@ -28,14 +34,22 @@ public class TodoSaveController {
     }
 
     @Secured(SecurityRule.IS_AUTHENTICATED)
+    @Produces(MediaType.TEXT_HTML)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Post("/todo")
     HttpResponse<?> save(//@NonNull @NotNull @Valid TodoCreate todo,
                          @NonNull @NotBlank String task,
-                         @NonNull OAuthUser oAuthUser) {
+                         @NonNull OAuthUser oAuthUser,
+                         HttpRequest<?> request) {
         // Workaround due to lambda binding issue
         TodoCreate todo = new TodoCreate(task.startsWith("task=") ? task.substring("task=".length()) : task);
-        todoSaveService.save(todo, oAuthUser);
+        String todoId = todoSaveService.save(todo, oAuthUser);
+        if (TurboMediaType.acceptsTurboStream(request)) {
+            return HttpResponse.ok(TurboStream.builder()
+                    .template("/todo/_tr.html", Collections.singletonMap("todo", new Todo(todoId, todo.getTask())))
+                    .targetDomId("todos-rows")
+                    .append());
+        }
         return HttpResponse.seeOther(UriBuilder.of("/todo").build());
     }
 }
